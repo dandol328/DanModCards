0d6f04ca546269eb4bcac34f0015613c424ccf4f
Dan <93361302+dandol328@users.noreply.github.com>
2026-04-21 10:02:28 -0700

    Refactor DoublePenetration class structure and methods

--- Cards/DoublePenetration.cs
+++ Cards/DoublePenetration.cs
@@ -3,92 +3,67 @@ using UnityEngine;
 
 namespace DanModCards.Cards
 {
-    class DoublePenetration : CustomCard
+    public class DoublePenetration : CustomCard
     {
-        public override void SetupCard(CardInfo cardInfo, Gun gun, ApplyCardStats cardStats, CharacterStatModifiers statModifiers)
-        {
-            // Fire 2 bullets per shot
-            gun.numberOfProjectiles += 1;
-
-            // Slight spread so they don't stack perfectly
-            gun.spread += 0.05f;
-
-            // Increased damage
-            gun.damage *= 1.25f;
-
-            // Longer reload
-            gun.reloadTime *= 1.5f;
-        }
-
-        public override void OnAddCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers statModifiers)
-        {
-        }
-
-        public override void OnRemoveCard(Player player, Gun gun, GunAmmo gunAmmo, CharacterData data, HealthHandler health, Gravity gravity, Block block, CharacterStatModifiers statModifiers)
-        {
-        }
-
-        protected override string GetTitle()
-        {
-            return "Double Penetration";
-        }
+        protected override string GetTitle() => "Double Penetration";
 
         protected override string GetDescription()
         {
             return "Fire two bullets in a tight volley\n<color=#00FF00>+25% damage</color>\n<color=#FF0000>+50% reload time</color>";
         }
 
-        protected override CardInfo.Rarity GetRarity()
+        protected override CardInfoStat[] GetStats() => new[]
         {
-            return CardInfo.Rarity.Uncommon;
-        }
-
-        protected override CardThemeColor.CardThemeColorType GetTheme()
-        {
-            return CardThemeColor.CardThemeColorType.OffensiveRed;
-        }
-
-        public override CardInfoStat[] GetStats()
-        {
-            return new CardInfoStat[]
+            new CardInfoStat
             {
-                new CardInfoStat()
-                {
-                    positive = true,
-                    stat = "Projectiles",
-                    amount = "+1",
-                    simepleAmount = CardInfoStat.SimpleAmount.notAssigned
-                },
-                new CardInfoStat()
-                {
-                    positive = true,
-                    stat = "Damage",
-                    amount = "+25%",
-                    simepleAmount = CardInfoStat.SimpleAmount.increased
-                },
-                new CardInfoStat()
-                {
-                    positive = false,
-                    stat = "Reload Time",
-                    amount = "+50%",
-                    simepleAmount = CardInfoStat.SimpleAmount.increased
-                }
-            };
+                positive = true,
+                stat = "Projectiles",
+                amount = "+1",
+                simepleAmount = CardInfoStat.SimpleAmount.notAssigned
+            },
+            new CardInfoStat
+            {
+                positive = true,
+                stat = "Damage",
+                amount = "+25%",
+                simepleAmount = CardInfoStat.SimpleAmount.increased
+            },
+            new CardInfoStat
+            {
+                positive = false,
+                stat = "Reload Time",
+                amount = "+50%",
+                simepleAmount = CardInfoStat.SimpleAmount.increased
+            }
+        };
+
+        protected override CardInfo.Rarity GetRarity() => CardInfo.Rarity.Uncommon;
+        protected override CardThemeColor.CardThemeColorType GetTheme() => CardThemeColor.CardThemeColorType.OffensiveRed;
+        protected override GameObject GetCardArt() => null;
+        public override string GetModName() => DanModCards.ModInitials;
+
+        public override void SetupCard(
+            CardInfo cardInfo, Gun gun, ApplyCardStats cardStats,
+            CharacterStatModifiers statModifiers, Block block)
+        {
+            gun.numberOfProjectiles += 1;
+            gun.spread += 0.05f;
+            gun.damage *= 1.25f;
+            gun.reloadTime *= 1.5f;
         }
 
-        protected override GameObject GetCardArt()
+        public override void OnAddCard(
+            Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block,
+            CharacterStatModifiers characterStats)
         {
-            return null;
         }
 
-        protected override CardInfoStat[] GetHiddenStats()
+        public override void OnRemoveCard(
+            Player player, Gun gun, GunAmmo gunAmmo, CharacterData data,
+            HealthHandler health, Gravity gravity, Block block,
+            CharacterStatModifiers characterStats)
         {
-            return null;
-        }
-
-        public override string GetModName()
-        {
-            return "DanMod";
         }
     }
 }
